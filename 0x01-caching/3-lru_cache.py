f0c78aa9c365a64908766f1aaf6edebddf5ecb26
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-31 21:18:42 +0200

    3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ LRUCache module """
 from base_caching import BaseCaching
+from collections import defaultdict
 
 
 class LRUCache(BaseCaching):
@@ -9,24 +10,20 @@ class LRUCache(BaseCaching):
     def __init__(self):
         """init method"""
         super().__init__()
-        self.usage = {}
+        self.usage = defaultdict(int)
 
     def put(self, key, item):
         """method to assign to the dictionary"""
         if key is None or item is None:
             return
-
-        if key in self.usage:
-            self.usage[key] += 1
-        else:
-            self.usage[key] = 1
-
+        self.cache_data[key] = item
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             min_key = min(self.usage, key=self.usage.get)
             del self.cache_data[min_key]
             del self.usage[min_key]
             print(f"DISCARD: {min_key}")
-        self.cache_data[key] = item
+
+        self.usage[key] += 1
 
     def get(self, key):
         """method to retreive from the dictionary"""
