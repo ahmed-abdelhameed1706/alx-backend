7acf6d6c659e3e48e2a72edb57cc6fcb25a7a3ca
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-02-06 22:57:50 +0200

    task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" basic app """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Config class"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@app.route("/")
+def index():
+    """index function"""
+    return render_template("3-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """get locale function"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
