be0b7e366dbafcd7f59a1ff141a863b31038c50a
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-02-06 23:08:26 +0200

    4-app.py

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" basic app """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Config class"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@app.route("/")
+def index():
+    """index function"""
+    return render_template("4-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """get locale function"""
+    if (
+        request.args.get("locale")
+        and request.args.get("locale") in app.config["LANGUAGES"]
+    ):
+        return request.args.get("locale")
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
